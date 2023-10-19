1c48514dc078ede9863b8563aecf92aa9767ab28
IRFAN FATHAN M <114319972+irfanfathan69@users.noreply.github.com>
2023-10-19 12:46:37 +0530

    mt fuctions

--- pi.py
+++ pi.py
@@ -1,6 +1,11 @@
 import math as mt
 mt.pi
 r=int(input("Enter the radius"))
-area=mt.pi*r*r
+area=mt.pi*mt.pow(r,2)
+print(mt.sin(120))
+print(mt.cos(120))
+print(mt.tan(120))
+print(mt.sqrt(120))
+print(mt.factorial(3))
 print(area)
       
