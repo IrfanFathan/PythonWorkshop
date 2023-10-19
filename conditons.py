f6e810c84f493f643ff89ec385fb3422881b40d9
IRFAN FATHAN M <114319972+irfanfathan69@users.noreply.github.com>
2023-10-19 15:49:21 +0530

    for loop

--- conditons.py
+++ conditons.py
@@ -1,8 +1,7 @@
 num1=int(input("ENTER AND A NUMBER"))
 num2=int(input("ENTER AND A NUMBER"))
-if num1 > num2:
-    print(f"{num1} is greater than {num2}")
-elif num1 < num2: 
-    print(f"{num2} is greater than {num1}")
+num3=int(input("ENTER AND A NUMBER"))
+if num1 >= num2 and num1 <= num3:
+    big=num1
 else: 
     print(f"{num1} and {num2} are equal")    
