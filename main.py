38b6dcfb028fa8b9bcd379ae1256b8f6e4f63823
IRFAN FATHAN M <114319972+irfanfathan69@users.noreply.github.com>
2023-10-19 12:09:08 +0530

    arthamatric claculation

---
+++ main.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the number 1"))
+num2=int(input("Enter the number 2"))
+add=num1+num2
+sub=num1-num2
+div=num1/num2
+multi=num1*num2
+print(add)
+print(sub)
+print(div)
+print(multi)
